7cad07eb6501f09d71ffd043350e03c74f7ca9a7
sceria <97571051+sceria@users.noreply.github.com>
2022-06-05 17:41:19 -0700

    File change
    Saved each operation result to a different register, instead of the same register.

--- Lab_4/OPtest.asm
+++ Lab_4/OPtest.asm
@@ -12,51 +12,51 @@ lw t1, datain_b
 add t2, t0, t1
 
 #and
-and t2, t0, t1
+and t4, t0, t1
 
 #or
-or t2, t0, t1
+or t5, t0, t1
 
 #sub
-sub t2, t0, t1
+sub t6, t0, t1
 
 #SLL by 1
 li t3, 1
-sll t2, t0, t3
+sll a1, t0, t3
 
 #SLL by 2
 li t3, 2
-sll t2, t0, t3
+sll a2, t0, t3
 
 #SLL by 3
 li t3, 3
-sll t2, t0, t3
+sll a3, t0, t3
 
 #SRL by 1
 li t3, 1
-srl t2, t0, t3
+srl a4, t0, t3
 
 #SRL by 2
 li t3, 2
-srl t2, t0, t3
+srl a5, t0, t3
 
 #SRL by 3 
 li t3, 3
-srl t2, t0, t3
+srl a6, t0, t3
 
 #--------------------IMMEDIATES	
 #addi
-addi t2, t0, 1
+addi a7, t0, 1
 
 #andi
-andi t2, t0, 1
+andi s2, t0, 1
 
 #ori
-ori t2, t0, 1
+ori s3, t0, 1
 
 #slli
-slli t2, t0, 1
+slli s4, t0, 1
 
 #srli
-srli t2, t0, 1
+srli s5, t0, 1
 
